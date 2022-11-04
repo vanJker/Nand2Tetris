9643a2a98239f7f71114e5b28c634207bc4e56de
Gshine <1847722163@qq.com>
2022-11-04 14:20:40 +0800

    Finish Project 4: Machine Language

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    @256
+    D=A         // D = 256
+    @R0
+    M=D         // R0 = 256
+    @SCREEN
+    D=A
+    @R2
+    M=D         // R2 = SCREEN
+    @KBD
+    D=M         // D = KBD
+    @CLEAR
+    D;JEQ       // If KBD == 0, goto CLEAR
+(BLACK)
+    @R0
+    MD=M-1      // D = --R0
+    @LOOP
+    D;JLT       // If R0 < 0, goto LOOP
+    @32
+    D=A         // D = 32
+    @R1
+    M=D         // R1 = 32
+(BLOOP)
+    @R1
+    MD=M-1      // D = --R1
+    @BLACK
+    D;JLT       // If R1 < 0, goto BLACK
+    @R2
+    D=M
+    M=M+1       // advance address of screen
+    A=D         // update A's content
+    M=-1        // -1 for 16-bit 1
+    @BLOOP
+    0;JMP
+(CLEAR)
+    @R0
+    MD=M-1      // D = --R0
+    @LOOP
+    D;JLT       // If R0 < 0, goto LOOP
+    @32
+    D=A         // D = 32
+    @R1
+    M=D         // R1 = 32
+(CLOOP)
+    @R1
+    MD=M-1      // D = --R1
+    @CLEAR
+    D;JLT       // If R1 < 0, goto CLEAR
+    @R2
+    D=M
+    M=M+1       // advance address of screen
+    A=D         // update A's content
+    M=0         // 0 for 16-bit 0
+    @CLOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,21 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @R2
+    M=0     // R2 = 0
+(LOOP)
+    @R1
+    D=M     // D = R1
+    @END
+    D;JEQ   // If R1 == 0, goto END
+    @R0
+    D=M     // D = R0
+    @R2
+    M=D+M
+    @R1
+    M=M-1   // R1 -= 1
+    @LOOP
+    0;JMP   // Goto LOOP
+(END)
+    @END
+    0;JMP
